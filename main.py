6fb8e2310c905b93404a269bb38f47a3e07a1cd0
Claudia <claudia.decarlo@studenti.unipd.it>
2025-05-24 15:18:03 +0200

    add UI for best recipes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.write("Welcome! Use the left menu to explore all functionalities.")
 
 ingredientsName = list(ing_map.keys())
 
-portions = st.slider("Number of portions", 1, 10, 2)
+portions = st.slider("Number of portions", 1, 10, 1)
 max_time = st.slider("Max prep time (min)", 5, 60, 30)
 buyIng = st.checkbox("Buy ingredients")
 unwantedIngName = st.multiselect("Allergies", ingredientsName)
@@ -25,9 +25,34 @@ preferredIngName = st.multiselect("Favourite Ingredients", ingredientsName)
 unwantedIng = [ing_map[n] for n in unwantedIngName]
 preferredIng = [ing_map[n] for n in preferredIngName]
 
+rank_emojis = ["🥇", "🥈", "🥉"] + ["🏅"] * 7
+
 if st.button("Suggest recipes"):
-    best = best_recipes(myPantry, myRecipes, preferredIng, unwantedIng, not buyIng, portions, max_time)
-    for name, score in best.items():
-        st.write(f" {name} — Score: {score}")
-    if len(best)==0:
-        st.success(f"❌ No recipes found with the filters selected.")
+    best = best_recipes(myPantry, myRecipes, preferredIng, unwantedIng, buyIng, portions, max_time)
+    st.markdown(f"""## Best recipes""")
+    if not best:
+        st.warning("❌ No recipes found with the filters selected.")
+    else:
+        for idx, (name, score) in enumerate(best.items(), start=1):
+            emoji = rank_emojis[idx - 1] if idx <= len(rank_emojis) else f"{idx}."
+            recipe = next(r for r in myRecipes if r.name == name).scaled_portions(portions)
+            st.markdown(f"""
+            <div style='font-size:20px; font-weight:600; margin-top:1em'>
+                {emoji} {recipe.name}
+            </div>
+            <div style='font-size:14px; color:gray; margin-bottom:0.5em'>
+                Score: {score}
+            </div>
+            """, unsafe_allow_html=True)
+
+            with st.expander("*Show details*"):
+                st.markdown("📝 ***Ingredients:***")
+                for ing in recipe.ingredients:
+                    st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;• {ing.name}: {ing.amount} {ing.unit}",
+                                unsafe_allow_html=True)
+
+                st.markdown(f"""  
+                    ⏰ ***Preparation time:*** **{recipe.prep_time}** min  
+
+                    ℹ️ ***Description:***  
+                    {recipe.description}""")
